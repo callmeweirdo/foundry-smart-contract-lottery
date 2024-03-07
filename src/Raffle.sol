5d8299ed66f917a0f9ff833801de46994b64cb6f
ogweirdo <54372821+callmeweirdo@users.noreply.github.com>
2024-03-07 00:07:47 +0000

    added a Raffle contract

---
+++ src/Raffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/** 
+    @title A sample Raffle contract
+    @author David Joseph
+    @notice This cotract is for creating sample raffle Draws
+    @dev Implements Chainlink VRFv2
+ */
+
+contract Raffle{
+
+}
