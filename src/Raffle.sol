11a322891a1b26ce40727d6fd9919f4bced6dd84
ogweirdo <54372821+callmeweirdo@users.noreply.github.com>
2024-04-14 10:14:52 +0000

    setting up the contract for tyhe chainlink VR functionlaityF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,22 +10,48 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle{
-    uint256 private immutable i_entranceFee; 
+    error Raffle_NotEnoughEthSent();
 
-    constructor(uint256 entranceFee){
+    uint256 private immutable i_entranceFee; 
+    // @dev duration of he contract in sec
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+
+    uint256 private s_lastTimeStamp;
+
+    event enteredRaffle(address indexed players);
+    
+
+    constructor(uint256 entranceFee, uint256 interval){
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() public payable {
+        if(msg.value < i_entranceFee){
+            revert Raffle_NotEnoughEthSent();
+        }
 
+        s_players.push(payable(msg.sender));
+        // 1. makes migratuon easier
+        // 2. makes frontend indexing easier
+        emit enteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {
-        
+    // 1. get  random number
+    // 2. use the random number to pick a winner
+    // 3. be automatically called
+    function pickWinner() public view {
+        if((block.timestamp - s_lastTimeStamp ) < i_interval ){
+            revert();
+        }
     }
     /** Getter Function */
 
     function getEntranceFee() external view returns (uint256){
         return i_entranceFee;
     }
+
+    
 }
